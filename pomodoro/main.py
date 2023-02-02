09379319884f216f621e03f8ba4238ca4cc92106
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-02-02 18:24:20 +0530

    layout complete

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -12,14 +12,35 @@ LONG_BREAK_MIN = 20
 window = Tk()
 window.size()
 window.title("Pomodoro App")
-window.config(padx=100, pady=50)
+window.config(padx=100, pady=50, bg=YELLOW)
 icon = PhotoImage(file="icon.png")
 window.iconphoto(False, icon)
 
+# Timer label:
 
-canvas = Canvas(width=200, height=224)
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+timer_label.grid(row=0, column=1)
+
+# Pomodoro countdown and image:
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 pomodoro_img = PhotoImage(file="tomato.png")
-canvas.create_image(102, 112, image=pomodoro_img)
-canvas.pack()
+canvas.create_image(100, 112, image=pomodoro_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(row=1, column=1)
+
+# Operations buttons:
+
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(row=3, column=0)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(row=3, column=2)
+
+# Check marks:
+
+check_label = Label(text="✔", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 15, "bold"))
+check_label.grid(row=4, column=1)
+
 
 window.mainloop()
