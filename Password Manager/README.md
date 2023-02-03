7f31fc3f51c49b822080705a2e0a0a13bc469800
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-02-03 11:04:28 +0530

    initial layout

---
+++ Password Manager/README.md
@@ -0,0 +1,3 @@
+Credits:
+
+<a href="https://www.flaticon.com/free-icons/passkey" title="passkey icons">Passkey icons created by zero_wing - Flaticon</a>
--- Password Manager/main.py
+++ Password Manager/main.py
@@ -1,5 +1,30 @@
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+from tkinter import *
+import math
+import random
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+# Setting up screen:
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
 
-# ---------------------------- UI SETUP ------------------------------- #
+# Adding an icon:
+icon = PhotoImage(file="icon.png")
+window.iconphoto(False, icon)
+
+# Adding the logo:
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+password_manager_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=password_manager_img)
+canvas.grid(row=1, column=1)
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
