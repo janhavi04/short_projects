f19defb6c556e60022775be50c4c026295460ed3
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-02-03 11:39:06 +0530

    UI done

--- Password Manager/main.py
+++ Password Manager/main.py
@@ -5,7 +5,7 @@ import random
 # Setting up screen:
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 # Adding an icon:
 icon = PhotoImage(file="icon.png")
@@ -15,13 +15,32 @@ window.iconphoto(False, icon)
 canvas = Canvas(width=200, height=200, highlightthickness=0)
 password_manager_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=password_manager_img)
-canvas.grid(row=1, column=1)
+canvas.grid(row=0, column=1)
+
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+
+website_entry = Entry(width=50)
+website_entry.grid(row=1, column=1, columnspan=2)
+
+email_username_label = Label(text="Email/Username:")
+email_username_label.grid(row=2, column=0)
+
+email_username_entry = Entry(width=50)
+email_username_entry.grid(row=2, column=1, columnspan=2)
+
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+password_entry = Entry(width=32)
+password_entry.grid(row=3, column=1)
 
 
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column=2)
 
-
-
-
+add_button = Button(text="Add", width=43)
+add_button.grid(row=4, column=1, columnspan=2)
 
 
 
