663e32bd60c7183808001cec5a36193dd9bd118e
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-02-04 08:31:33 +0530

    Initial layout

---
+++ Flash card/README.md
@@ -0,0 +1,4 @@
+Credit:
+
+Icon:
+<a href="https://www.flaticon.com/free-icons/extensible-markup-language" title="extensible markup language icons">Extensible markup language icons created by Freepik - Flaticon</a>
--- Flash card/main.py
+++ Flash card/main.py
@@ -1,2 +1,51 @@
+from tkinter import *
+import random
+
+# APP CONSTANTS:
 BACKGROUND_COLOR = "#B1DDC6"
 
+window = Tk()
+window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
+window.title("Flash Card")
+
+
+# Images and icons:
+
+icon = PhotoImage(file="images/icon.png")
+window.iconphoto(False, icon)
+
+card_back = PhotoImage(file="images/card_back.png")
+card_front = PhotoImage(file="images/card_front.png")
+cross_image = PhotoImage(file="images/wrong.png")
+check_image = PhotoImage(file="images/right.png")
+
+
+# Canvas:
+canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
+canvas.create_image(400, 263, image=card_front)
+canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+canvas.create_text(400, 263, text="French", font=("Ariel", 60, "bold"))
+canvas.grid(row=0, column=0, columnspan=2)
+
+
+# Buttons:
+
+unknown_button = Button(image=cross_image, highlightthickness=0)
+unknown_button.grid(row=1, column=0)
+known_button = Button(image=check_image, highlightthickness=0)
+known_button.grid(row=1, column=1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
