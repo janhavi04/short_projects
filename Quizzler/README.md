9f7051946ca65a363f976705948d6a744771ac8a
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-02-05 16:39:58 +0530

    edit quiz logic

---
+++ Quizzler/README.md
@@ -0,0 +1,3 @@
+Icon:
+
+<a href="https://www.flaticon.com/free-icons/quiz" title="quiz icons">Quiz icons created by Freepik - Flaticon</a>
--- Quizzler/ui.py
+++ Quizzler/ui.py
@@ -1,29 +1,40 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 FONT = ("Arial", 20, "italic")
+SCORE_FONT = ("Arial", 15, "normal")
 
 
 class QuizInterface():
-    def __init__(self):
-
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
-        self.score_label = Label(text="Score:", fg="white", bg=THEME_COLOR, justify= RIGHT)
-        self.score_label.grid(row=0, column=1, ipady=20)
+        # Score card
+        self.score_label = Label(text="Score:", fg="white", bg=THEME_COLOR, font=SCORE_FONT)
+        self.score_label.grid(row=0, column=1)
+        # Canvas
         self.canvas = Canvas(width=300, height=250, highlightthickness=0, bg="white")
-        self.canvas.grid(row=1, column=0, columnspan=2)
-        self.canvas.create_text(150, 125, text="Trial text", font=FONT)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+        self.question_text = self.canvas.create_text(150, 125, text="Trial text", font=FONT, width=280,
+                                                     fill=THEME_COLOR)
+        # True/False Buttons:
         true_image = PhotoImage(file="images/true.png")
         self.true_button = Button(image=true_image, highlightthickness=0)
         self.true_button.grid(row=3, column=0)
         false_image = PhotoImage(file="images/false.png")
         self.false_button = Button(image=false_image, highlightthickness=0)
         self.false_button.grid(row=3, column=1)
+        # Icon:
         icon = PhotoImage(file="images/icon.png")
         self.window.iconphoto(False, icon)
-
+        # Calls the next_question() from quiz_brain.
+        self.get_next_question()
 
         self.window.mainloop()
 
+    def get_next_question(self):
+        q_next = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_next)
