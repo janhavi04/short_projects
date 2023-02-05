36e11388bfaa6becb553ef3dd623b60a97f91427
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-02-05 17:54:26 +0530

    buttons and scoreboard functional

--- Quizzler/ui.py
+++ Quizzler/ui.py
@@ -2,6 +2,8 @@ from tkinter import *
 from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
+WRONG_COLOR = "#F55050"
+RIGHT_COLOR = "#AACB73"
 FONT = ("Arial", 20, "italic")
 SCORE_FONT = ("Arial", 15, "normal")
 
@@ -22,19 +24,35 @@ class QuizInterface():
                                                      fill=THEME_COLOR)
         # True/False Buttons:
         true_image = PhotoImage(file="images/true.png")
-        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button = Button(image=true_image, highlightthickness=0, command=self.true_clicked)
         self.true_button.grid(row=3, column=0)
         false_image = PhotoImage(file="images/false.png")
-        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button = Button(image=false_image, highlightthickness=0, command=self.false_clicked)
         self.false_button.grid(row=3, column=1)
         # Icon:
         icon = PhotoImage(file="images/icon.png")
         self.window.iconphoto(False, icon)
         # Calls the next_question() from quiz_brain.
+
         self.get_next_question()
 
         self.window.mainloop()
 
     def get_next_question(self):
+        self.canvas.config(bg="white")
+        self.score_label.config(text=f"Score: {self.quiz.score}")
         q_next = self.quiz.next_question()
         self.canvas.itemconfig(self.question_text, text=q_next)
+
+    def true_clicked(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def false_clicked(self):
+        self.give_feedback(self.quiz.check_answer("False"))
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg=RIGHT_COLOR)
+        else:
+            self.canvas.config(bg=WRONG_COLOR)
+        self.timer = self.window.after(1000, self.get_next_question)
